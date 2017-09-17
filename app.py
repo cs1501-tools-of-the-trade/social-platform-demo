14f5b54f49d4569b1c95d20904f9701490e4ece6
Srikanth Chelluri <schellluri.developer@gmail.com>
2017-09-17 17:59:33 -0400

    templating

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Index page"
+	# TODO: check if user is logged in
+	return render_template('index.html')
+
+@app.route("/profile")
+def profile():
+	return "Profile page"
+
+@app.route("/login")
+def login():
+	return render_template('login.html')
+
+@app.route("/register")
+def register():
+	return "Registration page"
