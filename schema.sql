e65b90fc4a248e594b7347b76c6020904744896d
Nikhil Gupta <ng3br@virginia.edu>
2017-10-22 18:15:52 -0400

    Added CRUD API for User

--- schema.sql
+++ schema.sql
@@ -1,7 +1,9 @@
 drop table if exists user;
 create table user (
   user_id integer primary key autoincrement,
-  username text not null,
+  username text not null unique,
+  first_name text not null,
+  last_name text not null,
   email text not null,
   pw_hash text not null
 );
